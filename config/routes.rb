64407b36d92908716df18804b88a1d18a66636d9
Leo Thompson <leo.francis.thompson@gmail.com>
2023-08-18 12:30:33 +0200

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
-Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
 
   # Defines the root path route ("/")
-  # root "articles#index"
-end
+  # root "articles#index" -- is the default landing page when someone sees your site
+
+
+  Rails.application.routes.draw do
+    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+    root 'lists#index'
+
+
+    resources :lists do
+      resources :bookmarks, only: [:index, :new, :create]
+    end
+    resources :bookmarks, only: :destroy
+  end
